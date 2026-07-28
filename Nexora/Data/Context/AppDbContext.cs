ebc620d9f84f13f32116f6f6d9fa8bf96cea5498
Lucas Vieira <vieiratm01@hotmail.com>
2026-07-28 17:18:16 -0300

    init: Inicializando projeto com as dependencias e camadas implementadas

---
+++ Nexora/Data/Context/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Nexora.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+            
+        }
+
+
+    }
+}
---
+++ Nexora/Data/Migrations/20260728193820_CriarDataBase.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Nexora.Migrations
+{
+    /// <inheritdoc />
+    public partial class CriarDataBase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
