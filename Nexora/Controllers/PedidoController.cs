3722df62268ce43899cd7dccc78481c60c1e19f9
Lucas Vieira <vieiratm01@hotmail.com>
2026-09-01 23:12:15 -0300

    feat: criando fluxo de adicionar itens ao pedido

--- Nexora/Controllers/PedidoController.cs
+++ Nexora/Controllers/PedidoController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Nexora.Dtos.ItemPedido;
 using Nexora.Dtos.Pedido;
 using Nexora.Entities;
 using Nexora.Interfaces;
@@ -10,14 +11,14 @@ namespace Nexora.Controllers
     public class PedidoController : ControllerBase
     {
         private readonly IPedidoService _pedidoService;
-        public PedidoController(IPedidoService pedidoService) 
+        public PedidoController(IPedidoService pedidoService)
         {
             _pedidoService = pedidoService;
         }
 
         [HttpPost]
         [Route("pedido")]
-        public async Task<ActionResult<Pedido>> CadastrarNovoPedido([FromBody]PedidoCreate pedidoDto, [FromQuery] int? usuarioId)
+        public async Task<ActionResult<Pedido>> CadastrarNovoPedido([FromBody] PedidoCreate pedidoDto, [FromQuery] int? usuarioId)
         {
             var pedido = await _pedidoService.CriarPedido(pedidoDto, usuarioId);
             return Created();
@@ -28,11 +29,25 @@ namespace Nexora.Controllers
         public async Task<ActionResult<PedidoResponse>> ConsultarPedidoId(int id)
         {
             var pedido = await _pedidoService.ConsultarPedidoId(id);
-            if(pedido == null)
+            if (pedido == null)
             {
-               return NotFound();
+                return NotFound();
             }
             return Ok(pedido);
+        }
+        [HttpPost]
+        [Route("pedido/{id}/itens")]
+        public async Task<ActionResult<PedidoResponse>> AdicionarItemAoPedido(int id, [FromBody] ItemPedidoCreate itemPedidoDto)
+        {
+            try
+            {
+                var pedido = await _pedidoService.AdicionarItemAoPedido(id, itemPedidoDto);
+                return Ok(pedido);
+            }
+            catch (InvalidOperationException ex)
+            {
+                return UnprocessableEntity(ex.Message);
+            }
         }
     }
 }
